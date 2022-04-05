fc09352940c764c5bc8c22a7b446bc6a62d9ae7f
Avinash Uchadiya <avinashuchadiya786@gmail.com>
2022-04-05 16:07:46 +0530

    added info to create a table in db.sql

--- db/db.sql
+++ db/db.sql
@@ -23,13 +23,22 @@
 
 -- to DROP or DELETE the database ==== DROP DATABASE  database_name;
 
+CREATE TABLE restaurants (
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  location VARCHAR(50) NOT NULL,
+  price_range INT NOT NULL check(price_range >= 1 AND price_range <= 5)
+);
 
-    CREATE TABLE reviews (
-        id BIGSERIAL NOT NULL PRIMARY KEY,
-        restaurant_id  BIGINT NOT NULL REFERENCES restaurants(id),
-        name VARCHAR(50) NOT NULL,
-        review TEXT NOT NULL,
-        rating INT NOT NULL check(rating >=1 and  rating<=5) );
+-- INSERT INTO restaurants (name, location, price_range) VALUES ('mcd', 'NY', 3);
 
-        INSERT INTO reviews (restaurant_id, name , review , rating) values (2,'Poonam', 'Loved the food there',5 );
+CREATE TABLE reviews (
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  restaurant_id BIGINT REFERENCES restaurants(id) ON DELETE CASCADE NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  review TEXT NOT NULL,
+  rating INT NOT NULL check(rating >=1 AND rating <= 5)
+);
+
+-- SELECT * FROM restaurants LEFT JOIN (SELECT restaurant_id, COUNT(*), TRUNC(AVG(rating), 1) AS average_rating FROM reviews GROUP BY restaurant_id) reviews ON restaurants.id = reviews.restaurant_id;
     
